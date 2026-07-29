df6a3ed09f038a60054140a3bed284dc8f8c8ecc
candratmkt <candra.tmkt@gmail.com>
2026-07-29 14:41:38 +0700

    Create supabaseClient.js

---
+++ assets/js/supabaseClient.js
@@ -0,0 +1,7 @@
+// assets/js/supabaseClient.js
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
+
+const SUPABASE_URL = 'https://pqhnzcsmbytwiurchnzl.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBxaG56Y3NtYnl0d2l1cmNobnpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUyMjQ2NzIsImV4cCI6MjEwMDgwMDY3Mn0.B5nv0C75tqK0kdD7pB7NxXZGn8CWZz7HgR9mECyrLXE';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
